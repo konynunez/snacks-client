a86e14c54d698bd634aacfae2c8e17c3a7fc2ed1
Konynunez <konynunez@gmail.com>
2024-09-17 10:36:45 -0400

    created app, installed axios, create instance

---
+++ src/utils/axiosinstance.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_SNACK_API_URL,
+  timeout: 1000,
+  headers: { "api-key": process.env.NEXT_PUBLIC_SNACK_API_KEY },
+});
+
+export default instance;
