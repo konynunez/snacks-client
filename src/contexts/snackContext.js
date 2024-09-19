6fcc379ee62e2cec232723c8040c55619af096a0
Konynunez <konynunez@gmail.com>
2024-09-19 11:11:29 -0400

    fix errors

--- src/contexts/snackContext.js
+++ src/contexts/snackContext.js
@@ -1,6 +1,6 @@
 "use client";
 import { createContext, useState, useEffect, useContext } from "react";
-import snackAPI from "@utils/axiosInstance";
+import snackAPI from "@/utils/axiosInstance";
 // create the context
 const snackContext = createContext();
 
