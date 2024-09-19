1b846b14d536fc83b2ca0bf109185c5768dc25ec
Konynunez <konynunez@gmail.com>
2024-09-19 11:17:10 -0400

    fixig errors

--- src/utils/axiosinstance.js
+++ src/utils/axiosinstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SNACK_API_URL,
-  timeout: 1000,
+  timeout: 5000,
   headers: {
     "api-key": process.env.NEXT_PUBLIC_SNACK_API_KEY,
   },
