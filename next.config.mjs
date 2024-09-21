4b7701e1ebb7d956985a58b218d14d0fe65dcf25
Konynunez <konynunez@gmail.com>
2024-09-21 12:16:16 -0400

    edited next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const path = require("path");
 
-export default nextConfig;
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
